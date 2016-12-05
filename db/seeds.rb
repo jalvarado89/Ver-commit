8bc209c6153b1e453e413e28e758a6c239ba8f5b
jalvarado89 <tpizo@hotmail.com>
2016-12-04 22:58:45 -0600

    otro cambio

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ ClienteNaviera.create(Nombre: 'Kiwi', Telefono: 'erhtrh', Correo: 'egreh', navie
 Plantum.create(Nombre: 'Visa', Telefono: 'erhtrh', Correo: 'egreh', Direccion: 'rhwn', companies_id: 2)
 Predio.create(Nombre: 'Grant', Telefono: 'erhtrh', Correo: 'egreh', Direccion: 'rhwn', companies_id: 2)
 Route.create(Nombre: 'Visa-Limon', Precio_Empresa_2Ejes: 600, Precio_Empresa_3Ejes: 600, Precio_Chofer: 230000, companies_id: 2)
-Asignation.create(Num_Semana: 675, fecha: '2016-11-24', hora: '09:00', Activo: true, companies_id: 2, predios_id: 1, predios_id2: 1, plantum_id: 1, cliente_navieras_id: 1, routes_id: 1, navieras_id: 1)
+Asignation.create(Num_Semana: 675, fecha: '2016-11-24', hora: '09:00', Activo: true, company_id: 2, predios_id: 1, predios_id2: 1, plantum_id: 1, cliente_navieras_id: 1, routes_id: 1, navieras_id: 1)
 
 Driver.create(Nombre: 'Nacho', Cedula: '2-569-899', Telefono: '57567786')
 Truck.create(Placa: 'C-158219', Modelo: '2013', Marca: 'gergg', drivers_id: 1)
