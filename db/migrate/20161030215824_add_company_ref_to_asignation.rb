831ead48af197d4fb589a003c5a3d065cb817e3f
jalvarado89 <tpizo@hotmail.com>
2016-12-04 23:12:06 -0600

    otro cambio ++

--- db/migrate/20161030215824_add_company_ref_to_asignation.rb
+++ db/migrate/20161030215824_add_company_ref_to_asignation.rb
@@ -1,4 +1,4 @@
-class AddProviderRefToAsignation < ActiveRecord::Migration[5.0]
+class AddCompanyRefToAsignation < ActiveRecord::Migration[5.0]
   def change
     add_reference :asignations, :companies, foreign_key: true
   end
