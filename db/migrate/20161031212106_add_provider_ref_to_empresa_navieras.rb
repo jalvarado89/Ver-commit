492dc6869ca58ecbfb6500fee99ca69c0b234693
jalvarado89 <tpizo@hotmail.com>
2016-12-07 01:32:27 -0600

    cambios ++

--- db/migrate/20161031212106_add_provider_ref_to_empresa_navieras.rb
+++ db/migrate/20161031212106_add_provider_ref_to_empresa_navieras.rb
@@ -1,5 +1,5 @@
 class AddProviderRefToEmpresaNavieras < ActiveRecord::Migration[5.0]
   def change
-    add_reference :company, :navieras, foreign_key: true
+    add_reference :companies, :navieras, foreign_key: true
   end
 end
